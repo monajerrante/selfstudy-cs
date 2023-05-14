616c9c3fb82be5b574211cb31897c3c9d6feeaf3
Monaje Errante <103513049+monajerrante@users.noreply.github.com>
2023-05-14 06:22:21 +0200

    Create HelloWorld.java

---
+++ intro_cs_java/semana_1/pset/HelloWorld.java
@@ -0,0 +1,11 @@
+/******************************************************************
+ * HelloWorld.java
+ * Problem Set 1.
+ * Problem 0. Clasical "Hello World"
+ ******************************************************************/
+ 
+public class HelloWorld{
+    public static void main(String[] argv){
+        System.out.println("Hello, World");
+    }
+}

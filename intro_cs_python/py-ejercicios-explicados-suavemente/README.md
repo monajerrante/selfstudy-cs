f5f589eeadfda81abbea47ad262e217624827978
Monaje Errante <103513049+monajerrante@users.noreply.github.com>
2023-05-23 01:52:32 +0200

    Add files via upload

---
+++ intro_cs_python/py-ejercicios-explicados-suavemente/README.md
@@ -0,0 +1,4 @@
+Book : "Python Programming Exercises, Gently Explained"
+Libro: "Ejercicios de programación de Python, explicados suavemente."
+
+https://inventwithpython.com/pythongently/
---
+++ intro_cs_python/py-ejercicios-explicados-suavemente/hello_world.py
@@ -0,0 +1,23 @@
+Diseño de solución
+
+No hay un diseño sorprendente para la solución. Es un programa sencillo de cuatro líneas:
+
+1. Imprime el texto "¡Hola, mundo!"
+
+2. Escriba el texto "¿Cuál es su nombre?"
+
+3. Permita que el usuario ingrese su nombre y guárdelo en una variable.
+
+4. Escriba el texto "Hola", seguido de su nombre.
+
+El programa llama a la función print() de Python para mostrar valores de cadena en la pantalla. Como siempre, las comillas de la cadena no se muestran en la pantalla porque no forman parte del texto del valor de la cadena, sino que marcan dónde comienza y termina la cadena en el código fuente del programa.
+
+El programa llama a la función input() de Python para obtener la entrada del usuario desde el teclado y la almacena en una variable.
+
+
+Casos especiales y trampas
+
+
+Otras lecturas
+
+Un "¡Hola, mundo!" program es un buen programa de destino para crear al aprender un nuevo lenguaje de programación. La mayoría de los lenguajes tienen un concepto de secuencias estándar para programas de línea de comandos basados en texto. Estos programas tienen un flujo de texto de entrada y un flujo de texto de salida. El flujo de texto de salida que produce el programa aparece en la pantalla (a través de print() en Python). El flujo de texto de entrada que acepta el programa proviene del teclado (a través de input() en Python). El principal beneficio de las secuencias es que puede redirigirlas: la salida de texto puede ir a un archivo en lugar de a la pantalla, o la entrada de un programa puede provenir de la salida de otro programa en lugar del teclado. Puede obtener más información sobre la interfaz de línea de comandos, también llamada ventanas de terminal, en el libro gratuito "Más allá de las cosas básicas con Python" en https://inventwithpython.com/beyond/. Los programas de línea de comandos tienden a ser más simples que los programas gráficos, y puede encontrar casi cien de estos programas en el libro gratuito The Big Book of Small Python Projects en https://inventwithpython.com/bigbookpython/.

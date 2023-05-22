3f698344e984eface32b49fa086881373896f76e
Monaje Errante <103513049+monajerrante@users.noreply.github.com>
2023-05-23 01:42:04 +0200

    Add files via upload

---
+++ intro_cs_java/semana_1/ejemplos_libro/HelloWorldDialog.java
@@ -0,0 +1,12 @@
+/*************************************************************
+ * Libro: Setter "Java en los ejemplos y las tares (deberias)"
+ * listado 2. Creó "HelloWorld" utilizando libraria grafico.
+ ************************************************************/ 
+import javax.swing.*;
+
+public class HelloWorldDialog {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hola a todos!");
+    }
+}
